8af053b390fb20fbb59777478274cbb27456042a
Florian Hartung <florian.hartung5+github@gmail.com>
2025-04-28 20:53:12 +0200

    feat(cli): parse arguments

--- crates/debugger-cli/src/main.rs
+++ crates/debugger-cli/src/main.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use debugger_core::add;
 use envconfig::Envconfig;
 use log::{LevelFilter, info};
@@ -8,12 +11,22 @@ struct Config {
     pub log_level: LevelFilter,
 }
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    executable_path: PathBuf,
+}
+
 fn main() -> anyhow::Result<()> {
     // For development/testing only
     let _ = dotenvy::dotenv();
 
     let config = Config::init_from_env()?;
     env_logger::builder().filter_level(config.log_level).init();
+
+    let args = Args::parse();
+
+    info!("TODO: open {:?} now", args.executable_path);
 
     info!("1 + 2 = {}", add(1, 2));
 
